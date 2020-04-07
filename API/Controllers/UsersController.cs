a6c5d7cd30af8b2ec23a4c38ac14d7766064c17a
milton.amado@outlook.es <milton.amado@outlook.es>
2020-04-07 15:43:43 -0300

    Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetUser()
+        {
+
+            return Ok("ok");
+        }
+
+        [HttpGet]
+        [Route("GetUsera")]
+        public ActionResult GetUsera()
+        {
+
+            return Ok("false");
+        }
+
+        [HttpGet]
+        [Route("GetUserb")]
+        public ActionResult GetUserb()
+        {
+
+            return Ok("NULL");
+        }
+
+    }
+}
